09e131bc057ff349c9197563cd7d5155b1c7a7f2
mitrev <allexandur4o@gmail.com>
2023-10-24 13:01:44 +0300

    final fix maybe

--- zig-cache/o/fb0bed8a0698162cb978d93147aad9c4/dependencies.zig
+++
@@ -1,6 +0,0 @@
-pub const imports = struct {
-    pub const zigimg = @import("12208c69b00540fe3f38893800fb6c89a9fe27b4a687de5d0462e498a43db4caeec0");
-};
-pub const build_root = struct {
-    pub const zigimg = "/home/mitrev/.cache/zig/p/12208c69b00540fe3f38893800fb6c89a9fe27b4a687de5d0462e498a43db4caeec0";
-};
